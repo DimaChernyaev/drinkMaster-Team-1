299d261de49c48ed26ddb04094aeddaf70ffd3c8
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-09-21 17:05:44 +0300

    create DrinkPage components + add images

---
+++ src/components/DrinkPageImg/DrinkPageImg.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Image = styled.img`
+@media (min-width: 1440px) {
+ width: 631;
+}
+@media (min-width: 768px) {
+    width: 704;
+}
+@media (min-width: 375px) {
+    width: 335;
+}
+`

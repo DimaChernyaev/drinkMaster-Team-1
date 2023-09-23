89900ff2356234b84052c35a48923dbc98399475
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-09-23 20:14:57 +0300

    add req

--- src/components/Drink/DrinkIngredientsList/DrinkIngredientsList.jsx
+++ src/components/Drink/DrinkIngredientsList/DrinkIngredientsList.jsx
@@ -1,3 +1,19 @@
-export const DrinkIngredientsList = () => {
-    return <div>Drink Ingredients List</div>
-}
+import { Ingredient } from '../Ingredient/Ingredient';
+import { Title, IngredientList } from './DrinkIngredientsList.style';
+
+export const DrinkIngredientsList = ({ coctailInfo }) => {
+  const ingredients = coctailInfo !== null ? coctailInfo.ingredients : [];
+
+  return (
+    <>
+      <Title>Ingredients</Title>
+      <IngredientList>
+        {ingredients.map(({ title, measure, ingredientId }) => {
+          return (
+            <Ingredient name={title} number={measure} key={ingredientId} />
+          );
+        })}
+      </IngredientList>
+    </>
+  );
+};
--- src/components/Drink/DrinkPageHero/DrinkPageHero.jsx
+++ src/components/Drink/DrinkPageHero/DrinkPageHero.jsx
@@ -3,8 +3,13 @@ import PageTitle from '../../DefaultComponents/PageTitle/PageTitle';
 import { AddToFavoriteBtn } from '../AddToFavoriteBtn/AddToFavoriteBtn';
 import { ShortDescr, SubTitle } from './DrinkPageHero.style';
 
-export const DrinkPageHero = () => {
-  const [addedDrink, setAddedDrink] = useState(false);
+export const DrinkPageHero = ({ coctailInfo }) => {
+  const title = coctailInfo !== null ? coctailInfo.drink : '';
+  const glass = coctailInfo !== null ? coctailInfo.glass : '';
+  const alcoholic = coctailInfo !== null ? coctailInfo.alcoholic : '';
+  const description = coctailInfo !== null ? coctailInfo.description : '';
+
+  const [addedDrink, setAddedDrink] = useState(true);
 
   const handleOnClick = () => {
     setAddedDrink((prev) => !prev);
@@ -12,14 +17,11 @@ export const DrinkPageHero = () => {
 
   return (
     <>
-      <PageTitle title="Coctaile name" style={{ marginBottom: '10px' }}/>
-      <SubTitle>Highball glass / Non alcoholic</SubTitle>
-      <ShortDescr>
-        Just a Moonmint is a refreshing and minty cocktail that combines the
-        flavors of vodka, lime juice, and mint syrup. It is made by shaking
-        vodka, lime juice, mint syrup, and ice together and straining it into a
-        glass.
-      </ShortDescr>
+      <PageTitle title={title} style={{ marginBottom: '10px' }} />
+      <SubTitle>
+        {glass} / {alcoholic}
+      </SubTitle>
+      <ShortDescr>{description} </ShortDescr>
       <AddToFavoriteBtn
         text={addedDrink ? 'Add to favorite drinks' : 'Remove from favorites'}
         onClick={handleOnClick}

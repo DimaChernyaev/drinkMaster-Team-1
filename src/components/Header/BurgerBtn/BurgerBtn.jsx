cebbb3075995d12d8f81155620a6489b7186aa98
Влад Язиков <vladzaraza@gmail.com>
2023-09-26 10:53:34 +0300

    Burger modal scroll lock

--- src/components/Header/BurgerBtn/BurgerBtn.jsx
+++ src/components/Header/BurgerBtn/BurgerBtn.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BurgerModal } from '../BurgerModal/BurgerModal';
 import { BurgerSVG, StyledBurgerBtn, CloseBurgerSVG } from './BurgerBtn.styled';
 
 export const BurgerBtn = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isModalOpen]);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);

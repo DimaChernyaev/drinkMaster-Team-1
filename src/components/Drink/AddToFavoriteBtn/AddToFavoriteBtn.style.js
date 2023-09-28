83b016baad32aa98584bfd2f741083fa69f6f3c5
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-09-28 16:58:10 +0300

    upgraded page

--- src/components/Drink/AddToFavoriteBtn/AddToFavoriteBtn.style.js
+++ src/components/Drink/AddToFavoriteBtn/AddToFavoriteBtn.style.js
@@ -2,34 +2,45 @@ import styled from 'styled-components';
 import { colorStyled } from '../../../helpers/colorStyled';
 
 export const Button = styled.button`
-
+  width: 222px;
   color: ${colorStyled.colorBlue};
   background-color: ${colorStyled.colorWhite};
   border-radius: 42px;
   border-color: transparent;
   font-size: 14px;
   font-weight: 600;
-  padding: 14px 40px;
+  padding-bottom: 14px;
+  padding-top: 14px;
   margin-bottom: 80px;
+  transition:
+    background-color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    color 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1);
 
   &:hover {
     background-color: ${colorStyled.colorBlue};
     color: ${colorStyled.colorWhite};
   }
-  &:focus {
+
+  /* &:focus {
     background-color: ${colorStyled.colorBlue};
     color: ${colorStyled.colorWhite};
+  }  */
+
+  &:active {
+    background-color: ${colorStyled.colorBlue};
+    color: ${colorStyled.colorWhite};
+    box-shadow: 0 0 10px ${colorStyled.colorWhiteFifty};
   }
 
   @media (min-width: 768px) {
+    width: 250px;
     font-size: 16px;
-    padding: 18px 44px;
-
+    padding-bottom: 18px;
+    padding-top: 18px;
   }
 
   @media (min-width: 1440px) {
-    font-size: 16px;
-    padding: 18px 44px;
     margin-bottom: 0;
   }
 `;

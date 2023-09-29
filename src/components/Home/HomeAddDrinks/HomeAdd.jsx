248953ff6da8a78f4063754bf1b96e321b66e6e2
Dima_ <chernyaev603@gmail.com>
2023-09-29 20:22:06 +0300

    fix

--- src/components/Home/HomeAddDrinks/HomeAdd.jsx
+++ src/components/Home/HomeAddDrinks/HomeAdd.jsx
@@ -1,35 +1,34 @@
 import hcss from '../../Home/home.module.css';
 import { Link } from 'react-router-dom';
-import img from '../../Home/HomePageImg/desktop2x.png'
+import img from '../../Home/HomePageImg/desktop2x.png';
 
 const HomeAdd = () => {
-return (
+  return (
     <div className={hcss.container}>
-     <div className={hcss.cont}>
+      <div className={hcss.cont}>
         {/* <div className={hcss.boxfour}></div> */}
         <div className={hcss.box}>
           <h1 className={hcss.title}>
-            Craft Your Perfect <b /> Drink with Drink Master
+            Craft Your Perfect <br /> Drink with Drink Master
           </h1>
           <p className={hcss.text}>
             Unlock your inner mixologist with Drink Master, your one-stop
             destination for exploring, crafting, and mastering the world´s
             finest beverages.
           </p>
-          
-          <Link className={hcss.btnAddDrinks} to="/add">Add drink</Link>
+
+          <Link className={hcss.btnAddDrinks} to="/add">
+            Add drink
+          </Link>
         </div>
-        <img className={hcss.imgmain} src={img} alt='main-coctail'/>
+        <img className={hcss.imgmain} src={img} alt="main-coctail" />
         <div className={hcss.boxtwo}></div>
         <div className={hcss.boxtree}></div>
         <div className={hcss.boxfour}></div>
-        <div>
-          
-        </div>
+        <div></div>
+      </div>
     </div>
-  </div>
-)
-
+  );
 };
 
 export default HomeAdd;
